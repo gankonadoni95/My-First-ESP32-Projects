5b30797f074cf810f0ed22d11cd8a93c6a8c7375
nurda <nurdauleterlanov545@gmail.com>
2026-06-30 21:51:10 +0500

    Add files via upload
    astronaut-heartRate-project-v1-Stanford
    added some code / from wokwi

--- sketch.ino
+++ sketch.ino
@@ -1,16 +1,34 @@
-byte blue_led = 14;
+byte blue_led = 27;
+byte red_led = 14;
+int heartRate;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(14, OUTPUT);
+  pinMode(blue_led, OUTPUT);
+  pinMode(red_led, OUTPUT);
 }
 
 void loop() {
-  int robotSpeeds[] = {50, 120, 200, 255};
-  for(int i=0; i<4; i++) {
-    analogWrite(14, robotSpeeds[i]);
-    delay(1000);
+  if (Serial.available() > 0) {
+    heartRate = Serial.parseInt();
+    
+    for(int i = 0; i < 10; i++) {
+
+      if (heartRate < 40 || heartRate > 161) {
+        digitalWrite(blue_led, LOW);
+        digitalWrite(red_led, HIGH);
+        delay(250);
+        digitalWrite(red_led, LOW);
+        delay(250);
+      }
+      else if (heartRate >= 41 || heartRate <= 160) {
+        digitalWrite(red_led, LOW);
+        digitalWrite(blue_led, HIGH);
+        delay(100);
+        digitalWrite(blue_led, LOW);
+        delay(100);
+      }
+    }
+    delay(3500);
   }
-  analogWrite(14, 0);
-  delay(5000);
 }
