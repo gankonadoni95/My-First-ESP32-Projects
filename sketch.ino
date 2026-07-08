be56bfe983ac10fa613a8019bccd077961432b73
nurda <nurdauleterlanov545@gmail.com>
2026-07-08 20:45:07 +0500

    Incessant-saver-project-v1
    Implemented non-blocking dual servo control using millis() timer

--- sketch.ino
+++ sketch.ino
@@ -1,41 +1,39 @@
-byte blue_led = 14;
-byte red_led = 27;
-float targetDistance = 300.5;
-bool isTargetHostile = true;
+#include <ESP32Servo.h>
+
+Servo leftServo;
+Servo rightServo;
+int heartRate = 85;
+unsigned long previousMillis = 0;
+const long interval = 1000;
+int systemStatus = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(blue_led, OUTPUT);
-  pinMode(red_led, OUTPUT);
+  leftServo.attach(18);
+  rightServo.attach(19);
 }
 
 void loop() {
-  if (Serial.available() > 0 ) {
-    targetDistance = Serial.parseFloat();
+  if(Serial.available() > 0) {
+    heartRate = Serial.parseInt();
+  }
 
-    if(targetDistance <= 250.5) {
-      isTargetHostile = true;
+  if(heartRate > 160 || heartRate < 50) {
+    systemStatus = 1;
+  } else {
+    systemStatus = 0;
+  }
+  unsigned long currentMillis = millis();
+
+  if(currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    if(systemStatus == 1) {
+      leftServo.write(90);
+      rightServo.write(90);
     } else {
-      isTargetHostile = false;
-  }
-}
-
-  if (isTargetHostile == true && targetDistance <= 150.5 ) {
-    digitalWrite(blue_led, HIGH);
-    digitalWrite(red_led, HIGH);
-    delay(200);
-    digitalWrite(red_led, LOW);
-    delay(200);
-  }
-  else if (isTargetHostile == true && targetDistance > 150.5 && targetDistance <= 250.5) {
-    digitalWrite(blue_led, LOW);
-    digitalWrite(red_led, HIGH);
-    delay(400);
-    digitalWrite(red_led, LOW);
-    delay(400);
-  }
-  else {
-    digitalWrite(blue_led, LOW);
-    digitalWrite(red_led, LOW);
+      leftServo.write(0);
+      rightServo.write(0); 
+    }
   }
 }
