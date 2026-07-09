77ddc4ad8227dfb8067f75253b0ad5b06a7f333b
nurda <nurdauleterlanov545@gmail.com>
2026-07-09 21:16:47 +0500

    multi-stage accident duration timer
    Added multi-stage accident duration timer using non-blocking millis;
    Created dual-interval non-blocking safety logic with custom loop checkpoints;
    Refactored safety logic with synchronized accident clock flags.

--- sketch.ino
+++ sketch.ino
@@ -1,16 +1,20 @@
 #include <ESP32Servo.h>
 
-Servo leftServo;
 Servo rightServo;
+Servo leftServo;
+byte red_led = 27;
 int heartRate = 85;
 unsigned long previousMillis = 0;
-const long interval = 1000;
+unsigned long accidentTimer = 0;
+const long firstInterval = 1000;
 int systemStatus = 0;
+bool accidentClockStarted = false;
 
 void setup() {
   Serial.begin(9600);
-  leftServo.attach(18);
   rightServo.attach(19);
+  leftServo.attach(18);
+  pinMode(27, OUTPUT);
 }
 
 void loop() {
@@ -18,22 +22,32 @@ void loop() {
     heartRate = Serial.parseInt();
   }
 
-  if(heartRate > 160 || heartRate < 50) {
+  if(heartRate > 160 || heartRate < 55) {
     systemStatus = 1;
+    
+    if(accidentClockStarted == false) {
+      accidentTimer = millis();
+      accidentClockStarted = true;
+    } 
   } else {
     systemStatus = 0;
+    accidentClockStarted = false;
+    digitalWrite(27, LOW);
   }
-  unsigned long currentMillis = millis();
 
-  if(currentMillis - previousMillis >= interval) {
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis >= firstInterval) {
     previousMillis = currentMillis;
 
     if(systemStatus == 1) {
-      leftServo.write(90);
       rightServo.write(90);
+      leftServo.write(90);
     } else {
-      leftServo.write(0);
-      rightServo.write(0); 
+      rightServo.write(0);
+      leftServo.write(0); 
     }
+  }
+  if(systemStatus = 1 && (currentMillis - accidentTimer >= 3000)) {
+    digitalWrite(27, HIGH);
   }
 }
