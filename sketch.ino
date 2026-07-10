eef32df7529f5bd92bb3846ad731daab87b41ca4
nurda <nurdauleterlanov545@gmail.com>
2026-07-10 20:07:15 +0500

    EES-StateMachine-v1.cpp
    Implemented asynchronous state machine with grouped logical operators

--- sketch.ino
+++ sketch.ino
@@ -1,53 +1,38 @@
 #include <ESP32Servo.h>
 
-Servo rightServo;
 Servo leftServo;
-byte red_led = 27;
-int heartRate = 85;
+Servo rightServo;
+int heartRate = 95;
+int systemMode = 0;
+byte pushButton = 4;
 unsigned long previousMillis = 0;
-unsigned long accidentTimer = 0;
-const long firstInterval = 1000;
-int systemStatus = 0;
-bool accidentClockStarted = false;
+const long interval = 1000;
 
 void setup() {
   Serial.begin(9600);
-  rightServo.attach(19);
   leftServo.attach(18);
-  pinMode(27, OUTPUT);
+  rightServo.attach(19);
+  pinMode(4, INPUT_PULLUP);
 }
 
 void loop() {
+  if(digitalRead(4) == LOW) {
+    systemMode = 1;
+  }
+
   if(Serial.available() > 0) {
     heartRate = Serial.parseInt();
   }
-
-  if(heartRate > 160 || heartRate < 55) {
-    systemStatus = 1;
-    
-    if(accidentClockStarted == false) {
-      accidentTimer = millis();
-      accidentClockStarted = true;
-    } 
-  } else {
-    systemStatus = 0;
-    accidentClockStarted = false;
-    digitalWrite(27, LOW);
-  }
-
   unsigned long currentMillis = millis();
-  if(currentMillis - previousMillis >= firstInterval) {
+  if(currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
 
-    if(systemStatus == 1) {
-      rightServo.write(90);
+    if(systemMode == 1 && (heartRate > 160 || heartRate < 55)) {
       leftServo.write(90);
+      rightServo.write(90);
     } else {
+      leftServo.write(0);
       rightServo.write(0);
-      leftServo.write(0); 
     }
-  }
-  if(systemStatus = 1 && (currentMillis - accidentTimer >= 3000)) {
-    digitalWrite(27, HIGH);
   }
 }
