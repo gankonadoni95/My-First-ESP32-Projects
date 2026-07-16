480a3d2ac60cbc96ba504ff97948b60f74856891
nurda <nurdauleterlanov545@gmail.com>
2026-07-16 20:55:09 +0500

    Implement emergency pedal control with servos
    Implemented non-blocking mechanical pedal emergency logic for Mark-4

---
+++ Mark4-PedalEmergency-v1.cpp
@@ -0,0 +1,52 @@
+#include <ESP32Servo.h>
+
+Servo leftServo;
+Servo rightServo;
+
+byte systemStatus = 0;
+byte pushButton = 4;
+byte red_led = 27;
+unsigned long previousMillis = 0;
+unsigned long accidentTimer = 0;
+const long Interval= 1000;
+bool accidentClockStarted = false;
+
+void setup() {
+  Serial.begin(9600);
+  leftServo.attach(18);
+  rightServo.attach(19);
+  pinMode(27, OUTPUT);
+  pinMode(4, INPUT_PULLUP);
+}
+
+void loop() {
+  if(digitalRead(4) == LOW) {
+    systemStatus = 1;
+
+    if(accidentClockStarted == false) {
+      accidentTimer = millis();
+      accidentClockStarted = true;
+    }
+  } else {
+    systemStatus = 0;
+    accidentClockStarted = false;
+    digitalWrite(27, LOW);
+  }
+
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis >= Interval) {
+    previousMillis = currentMillis;
+
+    if(systemStatus == 1) {
+    leftServo.write(90);
+    rightServo.write(90);
+  } else {
+    leftServo.write(0);
+    rightServo.write(0);
+    digitalWrite(27, LOW);
+  }
+}
+  if(systemStatus == 1 && (currentMillis - accidentTimer >= 3000)) {
+      digitalWrite(27, HIGH);
+ }
+}
